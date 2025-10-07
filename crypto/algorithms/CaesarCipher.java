99a403e59cfbb6ab825eba600834f732fbffc496
KARTHIKEYAN-EV <karthikeyan2410450@ssn.edu.in>
2025-10-07 18:24:05 +0530

    Added abstract class Cipher.java and updated the project structure

---
+++ crypto/algorithms/CaesarCipher.java
@@ -0,0 +1,9 @@
+package algorithms;
+public class CaesarCipher extends Cipher{
+	public String encrypt(String text,String key){
+		
+	}
+	public String decrypt(String text,String key){
+		
+	}
+}
---
+++ crypto/algorithms/Cipher.java
@@ -0,0 +1,19 @@
+package algorithms;
+public abstract class Cipher{
+	protected String algorithmName; //to store the name of each algorithm
+	protected static int encryptionCount=0;  //to count the encryption count
+	Cipher(String name){
+		this.algorithmName=name;
+	}
+	public String encrypt(String text,String key);
+	public String decrypt(String text,String key);
+	public String getAlgorithmName(){
+		return algorithmName;
+	}
+	protected void incrementEncryptionCount(){
+		encryptionCount++;
+	}
+	public int getEncryptionCount(){
+		return encryptionCount;
+	}
+}
