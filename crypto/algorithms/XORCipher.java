fa230902eecc382af1be1b2a6e69fc6fb56ee5ac
KARTHIKEYAN-EV <karthikeyan2410450@ssn.edu.in>
2025-10-07 19:32:52 +0530

    Updated XORCipher.java with implementation

--- crypto/algorithms/XORCipher.java
+++ crypto/algorithms/XORCipher.java
@@ -4,9 +4,16 @@ public class XORCipher extends Cipher{
 		super("XOR Cipher");
 	}
 	public String encrypt(String text,String key){
-		
+		StringBuilder result=new StringBuilder();
+		for (int i=0;i<text.length();i++){
+			char character=text.charAt(i);
+			char keyChar=key.charAt(i % key.length());
+			char encrypted=(char) (character ^ keyChar);  //XOR
+			result.append(encrypted);
+		}
+		return result.toString();
 	}
 	public String decrypt(String text,String key){
-		
+		return encrypt(text,key);   //XOR can be reversed by again encrypting it
 	}
 }
