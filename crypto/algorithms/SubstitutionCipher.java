47ce190f8d76cfe057ca08f21b22222378860336
Kaviya-1508 <kaviya.kavi15082006@gmail.com>
2025-10-08 10:20:37 +0530

    Updated /SubstitutionCipher.java

--- crypto/algorithms/SubstitutionCipher.java
+++ crypto/algorithms/SubstitutionCipher.java
@@ -1,14 +1,49 @@
 package algorithms;
-public class SubstitutionCipher extends Cipher{
+public class SubstitutionCipher extends Cipher {
 	SubstitutionCipher(){
 		super("Substitution Cipher");
 	}
-	public String encrypt(String text,String key){
-		StringBuilder result=new StringBuilder();
-		
-		return result.toString();
-	}
-	public String decrypt(String text,String key){
-		
-	}
+    private Map<Character, Character> encryptionMap;
+    private Map<Character, Character> decryptionMap;
+    public String encrypt(String text, String key) {
+        generateMaps(key);  // Use key to create mapping
+        StringBuilder result = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                result.append(encryptionMap.getOrDefault(c, c));
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+    public String decrypt(String text, String key) {
+        generateMaps(key);
+        StringBuilder result = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                result.append(decryptionMap.getOrDefault(c, c));
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+    private void generateMaps(String key) {
+        encryptionMap = new HashMap<>();
+        decryptionMap = new HashMap<>();
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String shuffled = shuffleAlphabet(key);  // Use key to shuffle alphabet
+        for (int i = 0; i < alphabet.length(); i++) {
+            char normal = alphabet.charAt(i);
+            char sub = shuffled.charAt(i);
+            encryptionMap.put(normal, sub);
+            decryptionMap.put(sub, normal);
+            // Add lowercase versions too
+            encryptionMap.put(Character.toLowerCase(normal), 
+                            Character.toLowerCase(sub));
+            decryptionMap.put(Character.toLowerCase(sub), 
+                            Character.toLowerCase(normal));
+        }
+    }
 }
